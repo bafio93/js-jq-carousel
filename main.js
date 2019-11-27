95af0aedf4f4067e879ddfb4ce736fde64448f8b
Previtali_Fabio <fabprev93@gmail.com>
2019-11-27 17:48:49 +0100

    Basic JS layout.

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-    $(".header-item > i").click(function(){
-        if ($(this).siblings(".submenu").hasClass("display-none")) {
-            $(".submenu").addClass("display-none");
-            $(this).siblings(".submenu").removeClass("display-none");
-        } else {
-            $(".submenu").addClass("display-none");
-        }
-        // Very wacky solution indeed!! XD
-    });
+    // Left section:
+    $("i.fa-arrow-circle-left").click(function(){
+        alert("Tutto ok left");
+    })
+    // Right section:
+    $("i.fa-arrow-circle-right").click(function(){
+        alert("Tutto ok right");
+    })
 });
