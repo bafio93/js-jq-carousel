6ab22e0ceea44e880eab8a6efc5a628935160c17
Previtali_Fabio <fabprev93@gmail.com>
2019-11-27 18:54:18 +0100

    Stuck in the dots section -.-" Taking a break!

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
-    // Left section:
+    // Arrow Left section:
     $("i.fa-arrow-circle-left").click(function(){
         var current_img = $("img.active");
+        var current_dot = $("i.active");
         var prev_img = current_img.prev("img");
+        var prev_dot = current_dot.prev("i");
         if (prev_img.length==0) {
             prev_img = $("img:last-child");
         }
+        if (prev_dot.length==0) {
+            prev_dot = $("dots-container i:last-child");
+        }
         current_img.removeClass("active");
         prev_img.addClass("active");
+        current_dot.removeClass("active");
+        prev_dot.addClass("active");
     })
-    // Right section:
+    // Arrow Right section:
     $("i.fa-arrow-circle-right").click(function(){
         var current_img = $("img.active");
         var next_img = current_img.next("img");
