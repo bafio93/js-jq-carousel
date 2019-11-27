e08cfaffa81780cf8fa0483dc5cbba6c95bf9264
Previtali_Fabio <fabprev93@gmail.com>
2019-11-27 17:55:14 +0100

    Semi-functional carousel. :)

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     // Left section:
     $("i.fa-arrow-circle-left").click(function(){
-        alert("Tutto ok left");
+        // alert("Tutto ok left");
+        var current_img = $("img.active");
+        var prev_img = $("img.active").prev("img");
+        current_img.removeClass("active");
+        prev_img.addClass("active");
     })
     // Right section:
     $("i.fa-arrow-circle-right").click(function(){
-        alert("Tutto ok right");
+        // alert("Tutto ok right");
+        var current_img = $("img.active");
+        var next_img = $("img.active").next("img");
+        current_img.removeClass("active");
+        next_img.addClass("active");
     })
 });
