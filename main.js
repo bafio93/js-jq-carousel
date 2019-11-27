9860b118fcbaa09e358eee434f47ccf1ab575e7b
Previtali_Fabio <fabprev93@gmail.com>
2019-11-27 18:03:33 +0100

    Working carousel!! :D

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
     // Left section:
     $("i.fa-arrow-circle-left").click(function(){
-        // alert("Tutto ok left");
         var current_img = $("img.active");
-        var prev_img = $("img.active").prev("img");
+        var prev_img = current_img.prev("img");
+        if (prev_img.length==0) {
+            prev_img = $("img:last-child");
+        }
         current_img.removeClass("active");
         prev_img.addClass("active");
     })
     // Right section:
     $("i.fa-arrow-circle-right").click(function(){
-        // alert("Tutto ok right");
         var current_img = $("img.active");
-        var next_img = $("img.active").next("img");
+        var next_img = current_img.next("img");
+        if (next_img.length==0) {
+            next_img = $("img:first-child");
+        }
         current_img.removeClass("active");
         next_img.addClass("active");
     })
