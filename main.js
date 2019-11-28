4f065ae057f2754451fd2c1da095c17718c0a582
Previtali_Fabio <fabprev93@gmail.com>
2019-11-28 09:48:14 +0100

    Done, without bonus! :D

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             prev_img = $("img:last-child");
         }
         if (prev_dot.length==0) {
-            prev_dot = $("dots-container i:last-child");
+            prev_dot = $(".dots-container i:last-child");
         }
         current_img.removeClass("active");
         prev_img.addClass("active");
@@ -19,11 +19,18 @@ $(document).ready(function(){
     // Arrow Right section:
     $("i.fa-arrow-circle-right").click(function(){
         var current_img = $("img.active");
+        var current_dot = $("i.active");
         var next_img = current_img.next("img");
+        var next_dot = current_dot.next("i");
         if (next_img.length==0) {
             next_img = $("img:first-child");
         }
+        if (next_dot.length==0) {
+            next_dot = $(".dots-container i:first-child")
+        }
         current_img.removeClass("active");
         next_img.addClass("active");
+        current_dot.removeClass("active");
+        next_dot.addClass("active");
     })
 });
